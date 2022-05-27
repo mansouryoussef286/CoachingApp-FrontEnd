34a4b5808b85c804d8491cf13b768ad81616b308
Yousif Whby <YousifWhby@gmail.com>
2022-05-27 20:39:18 +0200

    FeedBack component added

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { AboutGymawy } from './landingPageComponents/AboutGymawy'
+import { FeedBack } from './landingPageComponents/FeedBack';
 
 const LandingPage = () => {
     
     return (
+        <>
         <AboutGymawy></AboutGymawy>
+        <FeedBack></FeedBack>
+        </>
     );
 }
 export default LandingPage;
