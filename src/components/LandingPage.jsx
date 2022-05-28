3c7ad1cff4f789471d0f54ec4b99da67c07fbc13
Yousif Whby <YousifWhby@gmail.com>
2022-05-28 21:50:50 +0200

    coaches edit and modification

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,29 +1,24 @@
 import React from 'react'
+
 import { AboutGymawy } from './landingPageComponents/AboutGymawy'
-<<<<<<< HEAD
-import { Coaches } from './landingPageComponents/Coaches';
-=======
 import { Carousell } from './landingPageComponents/Carousell'
 import { Coaches } from './landingPageComponents/Coaches'
 import { Header } from './shared/Header.jsx'
 import { Footer } from './shared/Footer.jsx'
->>>>>>> 609a6ddc6b3c7da8f865d76c06e34b7fbb3169e0
 import { FeedBack } from './landingPageComponents/FeedBack';
 
 const LandingPage = () => {
 
-    return (
+        return (
         <>
         <Header></Header>
         <Carousell></Carousell>
         <AboutGymawy></AboutGymawy>
         <FeedBack></FeedBack>
-<<<<<<< HEAD
         <Coaches></Coaches>
-=======
         <Footer></Footer>
->>>>>>> 609a6ddc6b3c7da8f865d76c06e34b7fbb3169e0
+
         </>
-    );
+      );
 }
 export default LandingPage;
