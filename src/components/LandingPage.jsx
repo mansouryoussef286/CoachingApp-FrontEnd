9fb405bdd2f31914182643a389aece80c29f5c50
youssef mansour <mansouryoussef286@gmail.com>
2022-05-28 21:45:42 +0200

    adding coaches carousel in landing page, edit  not authorized

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,29 +1,25 @@
 import React from 'react'
-import { AboutGymawy } from './landingPageComponents/AboutGymawy'
-<<<<<<< HEAD
-import { Coaches } from './landingPageComponents/Coaches';
-=======
-import { Carousell } from './landingPageComponents/Carousell'
-import { Coaches } from './landingPageComponents/Coaches'
+
 import { Header } from './shared/Header.jsx'
 import { Footer } from './shared/Footer.jsx'
->>>>>>> 609a6ddc6b3c7da8f865d76c06e34b7fbb3169e0
+import { Carousell } from './landingPageComponents/Carousell'
+import { AboutGymawy } from './landingPageComponents/AboutGymawy'
 import { FeedBack } from './landingPageComponents/FeedBack';
+import { Coaches } from './landingPageComponents/Coaches'
+import { GetOurApp } from './landingPageComponents/GetOurApp.jsx'
 
 const LandingPage = () => {
 
-    return (
-        <>
-        <Header></Header>
-        <Carousell></Carousell>
-        <AboutGymawy></AboutGymawy>
-        <FeedBack></FeedBack>
-<<<<<<< HEAD
-        <Coaches></Coaches>
-=======
-        <Footer></Footer>
->>>>>>> 609a6ddc6b3c7da8f865d76c06e34b7fbb3169e0
-        </>
-    );
+        return (
+                <>
+                <Header></Header>
+                <Carousell></Carousell>
+                <AboutGymawy></AboutGymawy>
+                <FeedBack></FeedBack>
+                <Coaches></Coaches>
+                <GetOurApp></GetOurApp>
+                <Footer></Footer>
+                </>
+        );
 }
 export default LandingPage;
---
+++ src/components/landingPageComponents/GetOurApp.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const GetOurApp = () => {
+    return (
+        <div className=''></div>
+    )
+}
