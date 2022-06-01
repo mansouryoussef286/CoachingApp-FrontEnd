6223699a58c3a9eb6feca22d6cd43b5b78cb706e
youssef mansour <mansouryoussef286@gmail.com>
2022-06-01 21:32:40 +0200

    adding the cardcomponent which is a blank card then call it in any card component and add the specific body into it

--- src/components/ClientProfile.jsx
+++ src/components/ClientProfile.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { CardComponent } from './clientProfile/CardComponent'
+import { ProfileCard } from './clientProfile/ProfileCard'
 import { Sidebar } from './clientProfile/Sidebar'
 
 export const ClientProfile = () => {
     return (
         <>
         <Sidebar></Sidebar>
+        <div className='d-flex profile'>
+        <ProfileCard></ProfileCard>
+        <CardComponent></CardComponent>
+        </div>
         </>
     )
 }
---
+++ src/components/clientProfile/CardComponent.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { Rating } from 'primereact/rating';
+import { Card } from 'primereact/card';
+
+import { Dropdown } from 'react-bootstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
+export const CardComponent = (props) => {
+
+    
+    console.log(props.body);
+    return (
+        <div style={{ position: 'relative' }}>
+            <Dropdown autoClose='outside' drop='up' rootCloseEvent='click' className='coachCardContextMenu'>
+                <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
+                    <FontAwesomeIcon icon={faEllipsis} className='threedots' />
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu align={'end'} rootCloseEvent='mousedown' as={CustomMenu}>
+                    <Dropdown.Item eventKey="1">
+                        <Link to={'/'}>go to profile</Link>
+                    </Dropdown.Item>
+                    <Dropdown.Item eventKey="2">add to favourites</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+            <Card className='cardbox'>
+                {/* {props.body()} */}
+            </Card>
+        </div>
+    );
+}
+
+
+
+//#region three dots of sub menu
+// The forwardRef is important!!
+// Dropdown needs access to the DOM node in order to position the Menu
+const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <a
+    href=""
+    ref={ref}
+    onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+    }}
+    >
+    {children}
+    </a>
+));
+
+// forwardRef again here!
+// Dropdown needs access to the DOM of the Menu to measure it
+const CustomMenu = React.forwardRef(
+    ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
+    const [value, setValue] = useState('');
+
+    return (
+        <div
+        // ref={ref}
+        style={style}
+        className={className}
+        aria-labelledby={labeledBy}
+        >
+        <ul className="list-unstyled">
+            {React.Children.toArray(children).filter(
+            (child) =>
+                !value || child.props.children.toLowerCase().startsWith(value),
+            )}
+        </ul>
+        </div>
+    );
+    },
+);
+//#endregion
