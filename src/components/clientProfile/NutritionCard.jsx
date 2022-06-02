bbe1efd40794c9d6495136ee0ab38a3d8bf04cc3
ahmed <ahmed_elsokkary98@yahoo.com>
2022-06-02 17:18:59 +0200

    Nutrtion Card

--- src/components/clientProfile/NutritionCard.jsx
+++ src/components/clientProfile/NutritionCard.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { CardComponent } from './CardComponent'
 export const NutritionCard = () => {
+    const [day,setDay]=useState('Day 1');
+
+  const  handleChange=(e)=>{
+      
+       setDay(e.target.value)
+    }
+
+
+
     return (
         <CardComponent classes='workout-Card' >
             <div className='d-flex flex-column h-100'>
-                NutritionCard
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque rerum debitis dicta ducimus officiis at molestiae reiciendis earum aspernatur! Id laborum reiciendis illum, dicta labore adipisci quam porro veritatis repellat enim consectetur odit magnam. Veniam vel dicta molestiae, accusantium numquam iste beatae. Doloremque neque a sit officia, ut eos consequatur impedit nulla nisi autem doloribus dolorum quod voluptatibus ipsam molestias dolores similique architecto, repudiandae nostrum voluptas. Provident consequatur aliquam minima, autem saepe cumque quae ratione alias iste accusamus eaque voluptatibus facilis amet aperiam, illum, incidunt asperiores assumenda nam impedit maiores sunt distinctio ea quam? Asperiores inventore laborum est officiis odit sint natus voluptatibus animi adipisci repellendus repudiandae ducimus assumenda ullam voluptate eius, libero fugit, architecto cupiditate autem suscipit reprehenderit ad labore eligendi! Nulla hic quasi necessitatibus nisi maiores, quae temporibus, minus quisquam mollitia dolorem cum, soluta expedita sequi quidem praesentium molestias. Eos nesciunt amet laborum suscipit facilis eum corporis accusamus molestiae obcaecati inventore aut repellat beatae dolores aperiam quaerat quod, reprehenderit asperiores voluptatibus nisi tenetur expedita necessitatibus possimus delectus. Expedita, at tempore libero id quo veritatis ad consequatur architecto assumenda suscipit eius eligendi deserunt placeat eos dolorem numquam ratione odit quis voluptates. Quasi, reiciendis dolores ex laborum cupiditate aspernatur laudantium id optio exercitationem ratione nulla, numquam, laboriosam iusto explicabo dicta! Dolor quaerat nobis deserunt assumenda totam, iure a in! Reprehenderit, adipisci. Repellendus, officia quasi adipisci eos amet iusto reprehenderit ad fugiat commodi in expedita illum repudiandae illo, unde, aut numquam tenetur a quaerat? Quis expedita similique esse aliquid eius quaerat natus, repellendus, cupiditate ex culpa, in architecto voluptatem? Porro quisquam ipsum quia autem. Culpa fugit laudantium maxime sed. Nulla consequatur recusandae ratione dignissimos ipsum. Provident quia deserunt aspernatur magnam doloribus, deleniti, quod vero laboriosam, et ex voluptatum porro architecto molestiae doloremque eius laudantium sequi. Quasi nostrum possimus recusandae non impedit.
+            <select value={""} onChange={handleChange} style={{width:'130px'}} class="form-select" aria-label="Default select example">
+              <option >Select day</option>
+              <option >Day One</option>
+              <option >Day Two</option>
+              <option >Day Three</option>
+              <option >Day Four</option>
+              <option >Day Five</option>
+              <option >Day Six</option>
+              <option >Day Seven</option>
+             </select>
+               <h1 style={{textAlign:"center",marginBottom:'30px'}}>{day}</h1>
+               <div className='food'>
+                   <div className='meal'>
+                       <h3>BreakFast</h3>
+                       <img style={{width:'250px'}}  alt="Card"  src="assets/images/food.jpg" />
+                    <ul>
+                      <li>One grapefruit</li> 
+                      <li>Two poached eggs (or fried in a non-stick pan)</li>
+                      <li> Two slices whole-grain toast with one pat of butter each</li>
+                      <li>One cup low-fat milk</li>
+                      <li> One cup of black coffee or herbal tea</li>
+                    </ul>
+                   </div>
+                   <div className='meal'>
+                       <h3>Lunch</h3>
+                       <img style={{width:'250px'}}  alt="Card"  src="assets/images/food.jpg"  />
+                    <ul>
+                      <li>One grapefruit</li> 
+                      <li>Two poached eggs (or fried in a non-stick pan)</li>
+                      <li> Two slices whole-grain toast with one pat of butter each</li>
+                      <li>One cup low-fat milk</li>
+                      <li> One cup of black coffee or herbal tea</li>
+                    </ul>
+
+                   </div>
+                   <div className='meal'>
+                       <h3>Dinner</h3>
+                       <img style={{width:'250px'}}  alt="Card"  src="assets/images/food.jpg"  />
+                    <ul>
+                      <li>One grapefruit</li> 
+                      <li>Two poached eggs (or fried in a non-stick pan)</li>
+                      <li> Two slices whole-grain toast with one pat of butter each</li>
+                      <li>One cup low-fat milk</li>
+                      <li> One cup of black coffee or herbal tea</li>
+                    </ul>
+
+                   </div>
+
+                   <div className='meal'>
+                       <h3>Before Gym</h3>
+                       <img style={{width:'250px'}}  alt="Card"  src="assets/images/food.jpg" />
+                    <ul>
+                      <li>One Bnnana</li> 
+                      <li>1 scoop of creatine</li> 
+                    </ul>
+
+                   </div>
+
+               </div>
             </div>
         </CardComponent>
     )
