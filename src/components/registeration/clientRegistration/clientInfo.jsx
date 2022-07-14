1ce2083ff388d2ac173bdae39e84f33baf83d95a
ahmed <ahmed_elsokkary98@yahoo.com>
2022-07-14 17:09:43 +0200

    client register

--- src/components/registeration/clientRegistration/clientInfo.jsx
+++ src/components/registeration/clientRegistration/clientInfo.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import {ClientSignup2} from './clientInformation/clientSignup2'
+import { useLocation } from 'react-router'
+export const ClientInfo= (props) => {
+  const location = useLocation();
 
-export const ClientInfo= () => {
+ // console.log(location.state);
+
   return (
-    <ClientSignup2></ClientSignup2>
+
+    <ClientSignup2 data={location.state}></ClientSignup2>
   )
 }
