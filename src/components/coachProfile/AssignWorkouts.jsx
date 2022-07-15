eaf4a8bad160de43ea9f1c1181fe1d182633227e
youssef mansour <mansouryoussef286@gmail.com>
2022-07-15 16:38:36 +0200

    update json server with subscriptions coach and client api calls get requests in coach profile

--- src/components/coachProfile/AssignWorkouts.jsx
+++ src/components/coachProfile/AssignWorkouts.jsx
@@ -3,6 +3,8 @@ import { CardComponent } from './CardComponent'
 import { Dialog } from 'primereact/dialog';
 import { useState } from 'react';
 import { DialogAssignWorkout } from './DialogAssignWorkout';
+import { useFetch } from '../../useFetch';
+import Spinner from 'react-bootstrap/Spinner';
 
 export const AssignWorkouts = () => {
 
@@ -29,6 +31,37 @@ export const AssignWorkouts = () => {
         dialogFuncMap[`${name}`](false);
     }
 
+    const { data, loading, error, refetch } = useFetch('https://easyfit.azurewebsites.net/api/WSubscription/WorkoutSubsCoach');
+
+    if (loading) return (<div className='center-div'> <Spinner animation="grow" /></div>);
+    if (data == null) return (<div className='center-div'> <Spinner animation="grow" /></div>);
+    if (error) return (<h1>error...</h1>);
+
+    // console.log(data);
+
+    const printWorkouts = (clientSub) => {
+        return (clientSub.map((cSub) => {
+            if (cSub.accept) {
+                return (
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>{cSub.client.firstName} {cSub.client.lastName}</td>
+                        <td>{cSub.client.mobileNum}</td>
+                        <td>{cSub.acceptDate}</td>
+                        <td>
+                            <button className='btn btn-warning' label="Show" icon="pi pi-external-link" onClick={() => onClick('displayResponsive2')}>edit</button>
+                            <Dialog visible={displayResponsive2} onHide={() => onHide('displayResponsive2')} breakpoints={{ '960px': '75vw' }} style={{ width: '80vw' }} >
+                                <DialogAssignWorkout />
+                            </Dialog>
+                        </td>
+                    </tr>
+                )
+            }else{
+                return null
+            }
+        }))
+    }
+
     return (
         <CardComponent classes='workout-Card text-primary' >
             <h3>Assign Workouts to clients</h3>
@@ -37,22 +70,13 @@ export const AssignWorkouts = () => {
                     <tr>
                         <th scope="col">#</th>
                         <th scope="col">Name</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">operations</th>
+                        <th scope="col">Mobile</th>
+                        <th scope="col">Start date</th>
+                        <th scope="col"></th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto@gmail.khod</td>
-                        <td>
-                            <button className='btn btn-warning' label="Show" icon="pi pi-external-link" onClick={() => onClick('displayResponsive2')}>edit</button>
-                            <Dialog visible={displayResponsive2} onHide={() => onHide('displayResponsive2')} breakpoints={{ '960px': '75vw' }} style={{ width: '80vw' }} >
-                                <DialogAssignWorkout />
-                            </Dialog>
-                        </td>
-                    </tr>
+                    {printWorkouts(data)}
                 </tbody>
             </table>
         </CardComponent>
--- src/components/coachProfile/WorkoutCard.jsx
+++ src/components/coachProfile/WorkoutCard.jsx
@@ -3,6 +3,8 @@ import { CardComponent } from './CardComponent'
 import { Dialog } from 'primereact/dialog';
 import { DialogAddWorkout } from './DialogAddWorkout';
 import { DialogEditWorkout } from './DialogEditWorkout';
+import { useFetch } from '../../useFetch';
+import Spinner from 'react-bootstrap/Spinner';
 
 export const WorkoutCard = () => {
 
@@ -29,6 +31,34 @@ export const WorkoutCard = () => {
         dialogFuncMap[`${name}`](false);
     }
 
+    const { data, loading, error, refetch } = useFetch('https://easyfit.azurewebsites.net/api/Workout/Coach');
+
+    if (loading) return (<div className='center-div'> <Spinner animation="grow" /></div>);
+    if (data == null) return (<div className='center-div'> <Spinner animation="grow" /></div>);
+
+    if (error) return (<h1>error...</h1>);
+
+
+    // console.log(data);
+
+    const printWorkouts = (workouts) => {
+        return (workouts.map((workout) => {
+            return (<tr>
+                <th scope="row">1</th>
+                <td>{workout.name}</td>
+                <td>{workout.duration}</td>
+                <td>{workout.notes}</td>
+                <td>
+                    <button className='btn btn-warning' label="Show" icon="pi pi-external-link" onClick={() => onClick('displayResponsive2')}>edit</button>
+                    <button className='btn btn-danger' onClick={()=>{console.log(workout.id);}}>delete</button>
+                    <Dialog visible={displayResponsive2} onHide={() => onHide('displayResponsive2')} breakpoints={{ '960px': '75vw' }} style={{ width: '80vw' }} >
+                        <DialogEditWorkout />
+                    </Dialog>
+                </td>
+            </tr>)
+        }))
+    }
+
     return (
         <CardComponent classes='workout-Card text-primary' >
             <h3 >Workouts</h3>
@@ -43,24 +73,11 @@ export const WorkoutCard = () => {
                         <th scope="col">Name</th>
                         <th scope="col">Duration</th>
                         <th scope="col">Notes</th>
-                        <th scope="col">operations</th>
+                        <th scope="col"></th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>
-                            <button className='btn btn-warning' label="Show" icon="pi pi-external-link" onClick={() => onClick('displayResponsive2')}>edit</button>
-                            <button className='btn btn-danger'>delete</button>
-                            {/* <Dialog visible={displayBasic} style={{ width: '35vw' }} onHide={() => onHide('displayBasic')}> */}
-                            <Dialog visible={displayResponsive2} onHide={() => onHide('displayResponsive2')} breakpoints={{ '960px': '75vw' }} style={{ width: '80vw' }} >
-                                <DialogEditWorkout />
-                            </Dialog>
-                        </td>
-                    </tr>
+                    {printWorkouts(data)}
                 </tbody>
             </table>
         </CardComponent>
