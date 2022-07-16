9e5e42c4085a76d06b94c69d9946f15da61bff79
Mo-ismaiil <98978939+Mo-ismaiil@users.noreply.github.com>
2022-07-16 14:50:50 +0200

    hide profile button

--- src/components/landingPageComponents/Coaches.jsx
+++ src/components/landingPageComponents/Coaches.jsx
@@ -283,14 +283,14 @@ export const Coaches = () => {
                         <h4 className="mb-1">{coach.firstName} {coach.lastName} </h4>
                         <h6 className="mt-0 mb-3">Nutration</h6>
                         <span className={`coach-badge status-avialable`}>available</span>
-                        <div className="car-buttons mt-2">
+                        {/* <div className="car-buttons mt-2">
                             <Button icon="pi pi-search" className="p-button p-button-rounded mr-2" />
                             <Button icon="pi pi-star-fill" className="p-button-success p-button-rounded mr-2" />
-                            <Button icon="pi pi-Linkedin" className="p-button-help p-button-rounded" />
+                            <Button icon="pi pi-Linkedin" className="p-button-help p-button-rounded" /> */}
 
                             {/* <FontAwesomeIcon icon={faFacebookF} /> */}
-                        <div><a href='#' className='btn btn-primary mt-1'>Profile</a></div>
-                        </div>
+                        {/* <div><a href='#' className='btn btn-primary mt-1'>Profile</a></div> */}
+                        {/* </div> */}
                     </div>
                 </div>
             </div>
